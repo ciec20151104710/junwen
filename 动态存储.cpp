3c995aed0f33fe283fa660de6f9f99166f315918
ciec20151104710 <2471388619@qq.com>
2016-12-12 21:11:16 +0800

    Add files via upload

---
+++ 动态存储.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  静态存储
+//
+//  Created by s20151104710 on 16/10/17.
+//  Copyright © 2016年 s20151104710. All rights reserved.
+//
+
+#include <iostream>
+#include<stdio.h>
+#define N 5
+struct Node
+{
+    char name[50];
+    int age;
+    struct Node *next;
+};
+int main()
+{
+    struct Node * head;
+    struct Node a={"aaaaa",20,NULL},b={"bbbbb",21,NULL},c={"ccccc",22,NULL};
+    head=&a;
+    a.next=&b;
+    b.next=&c;
+    return 0;
+}
